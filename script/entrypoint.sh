1df57219149a981f445749fe5cb967095f653a81
Xinbin Huang <xbhuang93@hotmail.com>
2018-11-08 12:57:56 -0800

    Add LocalExecutor

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+POSTGRES_HOST="postgres"
+POSTGRES_PORT="5432"
+POSTGRES_USER="airflow"
+POSTGRES_PASSWORD="airflow"
+POSTGRES_DB="airflow"
+
+AIRFLOW__CORE__SQL_ALCHEMY_CONNECTION="postgresql+psycopg2://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}"
+
 # Generate Fernet key
 if [ -z $FERNET_KEY ]; then
     FERNET_KEY=$(python3 -c "from cryptography.fernet import Fernet; FERNET_KEY = Fernet.generate_key().decode(); print(FERNET_KEY)")
