48fb95698f702d18eaba8456cc9f9c3ba5a69e36
Xinbin Huang <xbhuang93@hotmail.com>
2018-11-08 16:58:01 -0800

    Update entrypoint.sh for LocalExecutor

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -1,19 +1,44 @@
 #!/usr/bin/env bash
 
+TRY_LOOP="10"
+
 POSTGRES_HOST="postgres"
 POSTGRES_PORT="5432"
 POSTGRES_USER="airflow"
 POSTGRES_PASSWORD="airflow"
 POSTGRES_DB="airflow"
 
-AIRFLOW__CORE__SQL_ALCHEMY_CONNECTION="postgresql+psycopg2://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}"
 
-# Generate Fernet key
-if [ -z $FERNET_KEY ]; then
-    FERNET_KEY=$(python3 -c "from cryptography.fernet import Fernet; FERNET_KEY = Fernet.generate_key().decode(); print(FERNET_KEY)")
+: ${AIRFLOW__CORE__FERNET_KEY:=$(python3 ./fernet_key.py)}
+: ${AIRFLOW__CORE__EXECUTOR:=${EXECUTOR:-Sequential}Executor}
+
+# set envrionment variables
+export \
+  AIRFLOW__CORE__FERNET_KEY \
+  AIRFLOW__CORE__SQL_ALCHEMY_CONN \
+  AIRFLOW__CORE__EXECUTOR
+
+
+wait_for_port() {
+  local name="$1" host="$2" port="$3"
+  local j=0
+  while ! nc -z "$host" "$port" >/dev/null 2>&1 < /dev/null; do
+    j=$((j+1))
+    if [ $j -ge $TRY_LOOP ]; then
+      echo >&2 "$(date) - $host:$port still not reachable, giving up"
+      exit 1
+    fi
+    echo "$(date) - waiting for $name... $j/$TRY_LOOP"
+    sleep 5
+  done
+}
+
+
+if [ ${AIRFLOW__CORE__EXECUTOR} != "SequentialExecutor" ]; then
+  AIRFLOW__CORE__SQL_ALCHEMY_CONN="postgresql+psycopg2://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB"
+  wait_for_port "Postgres" "$POSTGRES_HOST" "$POSTGRES_PORT"
 fi
 
-sed -i "s/{{ FERNET_KEY }}/${FERNET_KEY}/" ${AIRFLOW_HOME}/airflow.cfg
 
 case "$1" in
   webserver)
